07befc5e599b0998b3cd506f27245acf947df7e7
Olivia206 <olivia.simonet@mmibordeaux.com>
2023-05-05 10:08:03 +0200

    added style to block chapter alt

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@ import './vendors/lightbox';
 import './vendors/carousel';
 import './theme/';
 
-import './blocks/chapter';
 import './blocks/gallery';
 import './design-system/logo';
 import './design-system/footer';
